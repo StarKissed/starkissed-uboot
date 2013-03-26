8acf8a6dfaeef67ded87fb31e8be7a4a043371cd
TwistedUmbrella <TwistedUmbrella@gmail.com>
2013-03-26 01:14:47 -0400

    Add package generation without secondary kernel [Script needed]

---
+++ buildKernel.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (C) 2011 Twisted Playground
+
+# This script is designed by Twisted Playground for use on MacOSX 10.7 but can be modified for other distributions of Mac and Linux
+
+if cat /etc/issue | grep Ubuntu; then
+
+TOOLCHAIN_PREFIX=~/android/android-toolchain-eabi/bin
+KERNELSPEC=~/android/uboot-tuna
+MKBOOTIMG=$KERNELSPEC/buildImg/linux
+
+cd $KERNELSPEC/mkboot
+
+gcc -o mkbootfs mkbootfs.c
+
+gcc -c rsa.c
+gcc -c sha.c
+gcc -c mkbootimg.c
+gcc rsa.o sha.o mkbootimg.o -o mkbootimg
+rm *.o
+
+cp -R mkbootfs $MKBOOTIMG
+cp -R mkbootimg $MKBOOTIMG
+
+else
+
+TOOLCHAIN_PREFIX=/Volumes/android/android-toolchain-eabi/bin
+KERNELSPEC=/Volumes/android/uboot-tuna
+MKBOOTIMG=$KERNELSPEC/buildImg/darwin
+
+fi
+
+export PATH=$TOOLCHAIN_PREFIX:$PATH
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+
+make clean
+make distclean
+
+make omap4_tuna_config
+make -j8 omap4_tuna
+
+$MKBOOTIMG/./mkbootimg --kernel u-boot.bin --ramdisk /dev/null -o dualBoot/u-boot.img
+
+cd dualBoot
+rm *.zip
+zip -r "u-bootTuna.zip" *
